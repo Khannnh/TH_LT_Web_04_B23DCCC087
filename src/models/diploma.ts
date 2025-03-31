fde4094b17f11b1a6b48ab3ee426c99a33a31cfa
Khannnh <hhonksnguyen2205@gmail.com>
2025-03-31 23:13:31 +0700

    them comments

--- src/models/diploma.ts
+++ src/models/diploma.ts
@@ -1,51 +1,53 @@
-export interface DiplomaBook {
-    //id: string;     ko cần id vì mỗi một năm chỉ có 1 sổ văn bằng
+export interface DiplomaBook {    //sổ văn bằng
+    id: string;     //ko cần id vì mỗi một năm chỉ có 1 sổ văn bằng
     year: number;
     name: string;
-    currentSequence: number;
+    currentSequence: number; /*// Số thứ tự hiện tại 
+                            dùng để cấp số vào sổ cho văn bằng*/
     createdAt: string;
     updatedAt: string;
   }
   
-  export interface GraduationDecision {
+  export interface GraduationDecision {   // quyết định tốt nghiệp
     id: string;
-    decisionNumber: string; 
-    issueDate: string;
-    summary: string;
-    bookId: string;
-    searchCount: number;
+    decisionNumber: string; // số thứ tự của quyết định trong một năm cụ thể
+    issueDate: string; // Ngày ban hành quyết định
+    summary: string; //trích yếu của quyết định
+    bookId: string;// Thuộc sổ văn bằng nào (liên kết với DiplomaBook)
+    searchCount: number; //sổ lượt tra cứu quyết định tốt nghiệp
     createdAt: string;
     updatedAt: string;
   }
   
-  export interface FormField {
+  export interface FormField { //Cấu Hình Biểu Mẫu Phụ Lục Văn Bằng
     id: string;
-    name: string;
-    displayName: string;
-    dataType: 'string' | 'number' | 'date';
-    isRequired: boolean;
-    orderIndex: number;
+    name: string; //tên trường thông tin
+    displayName: string; //tên trường thông tin hiển thị cho người 
+    dataType: 'string' | 'number' | 'date'; //kiểu union type
+    isRequired: boolean; // Trường này có bắt buộc nhập không?
+    orderIndex: number; // Vị trí hiển thị trên giao diện
   }
   
   export interface Diploma {
-    id: string;
-    bookId: string;
-    decisionId: string;
-    sequenceNumber: number;
-    diplomaNumber: string;
+    id: string;// Mã văn bằng duy nhất
+    bookId: string;// Thuộc sổ văn bằng nào
+    decisionId: string;// Thuộc quyết định tốt nghiệp nào
+    sequenceNumber: number;//số vào sổ
+    diplomaNumber: string;// Số hiệu văn bằng
     studentId: string;
     fullName: string;
     birthDate: string;
-    fieldValues: {
-      [fieldId: string]: string | number | Date;
-    };
+    fieldValues: Record<string, string | number | Date>;
+
+    //key được lưu trữ dạng string 
+    //value có thể là 1 trong 3 kiểu dữ liệu
     createdAt: string;
     updatedAt: string;
   }
   
   export interface SearchCriteria {
-    diplomaNumber?: string;
-    sequenceNumber?: string;
+    diplomaNumber?: string;//số hiệu văn bằng
+    sequenceNumber?: string;//số vào sổ 
     studentId?: string;
     fullName?: string;
     birthDate?: string;
